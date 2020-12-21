8eea63686cad45fe0dbea0b4ca8603e39ed0fd70
Lazula <26179473+Lazula@users.noreply.github.com>
2020-12-21 16:54:22 -0600

    Add PJ to libr/bin/pdb/pdb_downloader.c

--- libr/bin/pdb/pdb_downloader.c
+++ libr/bin/pdb/pdb_downloader.c
@@ -171,10 +171,18 @@ static bool is_valid_guid(const char *guid) {
 }
 
 int r_bin_pdb_download(RCore *core, int isradjson, int *actions_done, SPDBOptions *options) {
+	PJ *pj;
 	int ret;
 	SPDBDownloaderOpt opt;
 	SPDBDownloader pdb_downloader;
 	RBinInfo *info = r_bin_get_info (core->bin);
+
+	if (isradjson) {
+		pj = pj_new ();
+		if (!pj) {
+			return 1;
+		}
+	}
 
 	if (!info || !info->debug_file_name) {
 		eprintf ("Can't find debug filename\n");
@@ -201,9 +209,13 @@ int r_bin_pdb_download(RCore *core, int isradjson, int *actions_done, SPDBOption
 	init_pdb_downloader (&opt, &pdb_downloader);
 	ret = pdb_downloader.download ? pdb_downloader.download (&pdb_downloader) : 0;
 	if (isradjson && actions_done) {
-		//TODO PJ
-		printf ("%s\"pdb\":{\"file\":\"%s\",\"download\":%s}",
-		        *actions_done ? "," : "", opt.dbg_file, ret ? "true" : "false");
+		pj_ko (pj, "pdb");
+		pj_ks (pj, "file", opt.dbg_file);
+		pj_kb (pj, "download", ret);
+		pj_end (pj);
+
+		printf ("%s%s", *actions_done ? "," : "", pj_string (pj));
+		pj_reset (pj);
 	} else {
 		printf ("PDB \"%s\" download %s\n",
 		        opt.dbg_file, ret ? "success" : "failed");
@@ -212,6 +224,10 @@ int r_bin_pdb_download(RCore *core, int isradjson, int *actions_done, SPDBOption
 		(*actions_done)++;
 	}
 	deinit_pdb_downloader (&pdb_downloader);
+
+	if (isradjson) {
+		pj_free (pj);
+	}
 
 	return !ret;
 }
